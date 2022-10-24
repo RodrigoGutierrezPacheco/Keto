8a321c1df1f3cdd43c365c5e8f2f01154810b3f4
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-10-24 14:19:30 -0500

    propuesta pagina inicial

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { height } from '@mui/system';
+import MessageIcon from '@mui/icons-material/Message';
+import { green, purple } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from 'styled-components';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
+export default function Footer() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+  <ThemeProvider theme={theme}>
+    <Box sx={{ width:'100%', alignItems:'center'}}>
+		 <BottomNavigationAction label="Messenger" icon={<MessageIcon />} />
+    </Box>
+		</ThemeProvider>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,119 @@ import "./HomePage.css";
 function HomePage() {
   return (
     <div>
-      <h1>Home page</h1>
+			<div>
+			<img className="dieta" src="images/dieta.jpg" alt="" />
+      <img className="dieta" src="images/dieta1.jpg" alt="" />
+			</div>
+			<div>
+				<h2>¿Qúe es la dieta Keto?</h2>
+				<iframe width="99%" height="40%" src="https://www.youtube.com/embed/7DRLUUUO10c" title="¿Qué es la dieta keto? Dieta cetogénica para principiantes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
+			<section>
+				<h2>¿Qué incluye?</h2>
+				<div className="queIncluye">
+					<div className="containerOne">
+						<img className="menu" src="images/menu.png" alt="" />
+						<p>Guia de Alimentacion</p>
+					</div>
+					<div className="containerOne">
+						<img className="menu" src="images/menu.png" alt="" />
+						<p>Recetas keto y jugos verders para complementar tu reto</p>
+					</div >
+					<div className="containerOne">
+						<img className="menu" src="images/menu.png" alt="" />
+						<p>Lista de alimentos de intercampo keto</p>
+					</div>
+					<div className="containerOne">
+						<img className="menu" src="images/menu.png" alt="" />
+						<p>Guia para salir de cetosis correctamente al final del reto</p>
+					</div>
+					<div className="containerOne">
+						<img className="menu" src="images/menu.png" alt="" />
+						<p>Guia de suplementacion correcta en cetosis</p>
+					</div>
+					<div className="containerOne">
+						<img className="menu" src="images/menu.png" alt="" />
+						<p>2 Semanas de inversion y nutricion optima para tu cuerpo</p>
+					</div>
+					<div className="containerOne">
+						<img className="menu" src="images/menu.png" alt="" />
+						<p>Menú "Keto to Go" faciles y keto en casa para tu reto</p>
+					</div>
+					<div className="containerOne">
+						<img className="menu" src="images/menu.png" alt="" />
+						<p>Marcas recomendadas</p>
+					</div>
+				</div>
+			</section>
+			<article>
+				<h2>Tienda en Linea</h2>
+				<div className="tienda">
+					 <blockquote className="tiendaOne">
+					<img className="comida" src="images/comida.jpg" alt="" />
+					<p>Nuestros Retos</p>
+					 </blockquote>
+					 <blockquote className="tiendaOne">
+					<img className="comida" src="images/comida.jpg" alt="" />
+					<p>Reto 21 dias</p>
+					 </blockquote>
+					 <blockquote className="tiendaOne">
+					<img className="comida" src="images/comida.jpg" alt="" />
+					<p>Consulta en linea</p>
+					 </blockquote>
+					 <blockquote className="tiendaOne">
+					<img className="comida" src="images/comida.jpg" alt="" />
+					<p>Recetarios</p>
+					 </blockquote>
+					 <blockquote className="tiendaOne">
+					<img className="comida" src="images/comida.jpg" alt="" />
+					<p>Productos Recomendados</p>
+					 </blockquote>
+					 <blockquote className="tiendaOne">
+					<img className="comida" src="images/comida.jpg" alt="" />
+					<p>Clases en linea</p>
+					 </blockquote>
+				</div>
+			</article>
+			<div>
+				<h2>Testimonios</h2>
+				<article className="testimonios">
+				<div className="testimonio">
+					<img src="images/testimonio1.jpg" alt="" />
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, officia? Excepturi, in optio. Dolorum itaque doloremque necessitatibus quibusdam iste, id reiciendis hic ut veritatis ipsam ratione eaque accusantium amet voluptates?</p>
+				</div>
+				<div className="testimonio">
+					<img src="images/testimonio1.jpg" alt="" />
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, officia? Excepturi, in optio. Dolorum itaque doloremque necessitatibus quibusdam iste, id reiciendis hic ut veritatis ipsam ratione eaque accusantium amet voluptates?</p>
+				</div>
+				</article>
+			</div>
+			<div className="dietaKeto">
+				<h2>¿Qué es la dieta cetogenica/keto?</h2>
+				<p>*Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores fuga quaerat sapiente commodi quam! Voluptatem eaque enim veritatis quas optio nihil sed, iusto laborum odio voluptas corrupti quod commodi praesentium!</p>
+				<p>*Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores fuga quaerat sapiente commodi quam! Voluptatem eaque enim veritatis quas optio nihil sed, iusto laborum odio voluptas corrupti quod commodi praesentium!</p>
+				<button className="buttonKeto">Entra al Reto!</button>
+			</div>
+			<article className="articulo">
+				<img src="images/articulo.jpg" alt="" />
+				<h3>Beneficios de la dieta Keto</h3>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, excepturi asperiores? Ullam atque blanditiis ut sit minima necessitatibus eaque deserunt quis! Repellat dolorem cupiditate voluptate est quas, mollitia quo ex?</p>
+				<a href="">Leer más--></a>
+			</article>
+			<article className="articulo">
+				<img src="images/articulo.jpg" alt="" />
+				<h3>Postres Keto</h3>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, excepturi asperiores? Ullam atque blanditiis ut sit minima necessitatibus eaque deserunt quis! Repellat dolorem cupiditate voluptate est quas, mollitia quo ex?</p>
+				<a href="">Leer más--></a>
+			</article>
+			<article className="articulo">
+				<img src="images/articulo.jpg" alt="" />
+				<h3>Dulces Keto</h3>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, excepturi asperiores? Ullam atque blanditiis ut sit minima necessitatibus eaque deserunt quis! Repellat dolorem cupiditate voluptate est quas, mollitia quo ex?</p>
+				<a href="">Leer más--></a>
+			</article>
+			<button className="buttonArticulos">Mas Articulos!</button>
+			<h1>KetoChallenge</h1>
     </div>
   );
 }
