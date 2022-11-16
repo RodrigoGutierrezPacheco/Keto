ade0106b4586cb70dbc561721e697fab5acd28b7
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-11-16 17:58:18 -0600

    modificacion footer e instalacion MDBbootstrap

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,49 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import { height } from '@mui/system';
-import { green, purple } from '@mui/material/colors';
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from 'styled-components';
-import './Footer.css'
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } 
+from "mdbreact";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: purple[500],
-    },
-    secondary: {
-      main: green[500],
-    },
-  },
-});
 
-export default function Footer() {
-  const [value, setValue] = React.useState(0);
 
+const FooterPage = () => {
   return (
-  <div className='footer'>
-  <div className='reds'>
-		<a href="https://facebook.com"><img className='redes' src="images/fb.png" alt="" /></a>
-		<a href="https://instagram.com"><img  className='redes' src="images/instagram.png" alt="" /></a>
-		<a href="https://messenger.com"><img  className='redes' src="images/messenger.png" alt="" /></a>
-		<a href="https://whatsapp.com"><img  className='redes' src="images/whatsapp.png" alt="" /></a>
-	</div>
-  </div>
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Keto Challenge</h5>
+            <p>
+							Somos un grupo de Profesionales que nos gusta ayudar a las personas a cumplir sus objetivos
+            </p>
+          </MDBCol>
+          <MDBCol md="6">
+            {/* <h5 className="title">Links</h5>
+            {/* <ul>
+              <li className="list-unstyled">
+                <a href="#!">Instagram</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 2</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 3</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 4</a>
+              </li>
+            </ul> */} 
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="/"> KetoChallenge </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
   );
 }
+
+export default FooterPage;
