a52151cf3238f7a7f373e199a625c8c29debc576
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-11-23 20:16:39 -0600

    modificacion navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,34 +4,52 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Color } from 'react-bootstrap/esm/types';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-
-function NavBar() {
+function OffcanvasExample() {
   return (
-    <Navbar sticky="top" className='navbar'  expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="/">
-						<img className='logonav' src="images/KetoChallengeLogo.png" alt="" />
-				</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px',color:'blue' }}
-            navbarScroll
-          >
-            <Nav.Link href="/"><p>Inicio</p></Nav.Link>
-            <Nav.Link href="/retos"><p>Retos</p></Nav.Link>
-            <Nav.Link href="/recetarios">
-              <p>Recetarios</p>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      {[false].map((expand) => (
+        <Navbar sticky='top' key={expand}  expand={expand} className="mb-3 navbar bgColor">
+          <Container fluid>
+            <Navbar.Brand href="/">Keto Challenge</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header className='bgColor' closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Menú
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/">Inicio</Nav.Link>
+                  <Nav.Link href="/retos">Retos</Nav.Link>
+                  <NavDropdown
+                    title="Recetarios"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="/recetario1">Recetario 1</NavDropdown.Item>
+                    <NavDropdown.Item href="/recetario2">
+                      Recetario 2
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/recetario3">
+                      Recetario 3
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Offcanvas.Body>
+							<img className="imgOff" src="images/ketoChallenge1.png" alt="" />
+
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
   );
 }
 
-export default NavBar;
+export default OffcanvasExample;
